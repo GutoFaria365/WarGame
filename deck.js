53da65fa8b68eebd79275cb8c1c8b047825e8514
joaogilferreira <joaogilferreira@hotmail.com>
2023-04-10 18:37:50 +0100

    Shuffled Deck

--- deck.js
+++ deck.js
@@ -15,19 +15,36 @@
     
     const suits = ['heart', 'club', 'diamond', 'spade']
     const values = [2,3,4,5,6,7,8,9,10,'j','q','k','a']
+    const deck = []
+    const shuffledDeck = [];
 
     function card(suit, value) {
         this.suit = suit;
         this.value = value;
     };
      
-    function creatDeck() {
-        const deck = []
+    function createDeck() {
         suits.forEach(suit => {
             values.forEach(value => {
                 deck.push(new card(suit, value));
             }); 
         });
-    }  
+    }
+
+/*     function shuffleDeck(deck) {
+        for (let i = deck.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [deck[i], deck[j]] = [deck[j], deck[i]];
+        }
+        return deck;
+      } */
+
+     function shuffleDeck(deck) {
+    
+        deck.forEach(card => {
+            const randomCard = Math.floor(Math.random() * deck.length)
+            shuffledDeck.push(deck[randomCard])
+        })
+    }
 
 
