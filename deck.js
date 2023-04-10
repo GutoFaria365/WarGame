10789b4d14b2e1bce3c1935385387f1ac114f754
GutoFaria365 <122914909+GutoFaria365@users.noreply.github.com>
2023-04-10 17:49:40 +0100

    we have a deck

--- deck.js
+++ deck.js
@@ -5,9 +5,29 @@
         var vBotImg = document.getElementById("botValues");
 
         var randomIndex = Math.floor(Math.random() * imageArray.length);
-        sTopImg.src = imageArray[randomIndex];
+        sTopImg.src = suit;
+        vTopImg.src = value;
         sBotImg.src = sTopImg.src;
-      
-    }
+        vBotImg.src = vTopImg.src;
 
+    }
     
+    
+    const suits = ['heart', 'club', 'diamond', 'spade']
+    const values = [2,3,4,5,6,7,8,9,10,'j','q','k','a']
+
+    function card(suit, value) {
+        this.suit = suit;
+        this.value = value;
+    };
+     
+    function creatDeck() {
+        const deck = []
+        suits.forEach(suit => {
+            values.forEach(value => {
+                deck.push(new card(suit, value));
+            }); 
+        });
+    }  
+
+
