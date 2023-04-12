560da8c02f84276ac3485418046908dfae9cc876
GutoFaria365 <122914909+GutoFaria365@users.noreply.github.com>
2023-04-12 13:24:38 +0100

    logic is functional

--- deck.js
+++ deck.js
@@ -13,8 +13,8 @@ function changeImage(imageArray) {
 }
 
 
-const suits = ['heart', 'club', 'diamond', 'spade']
-const values = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
+const suits = ["heart", "club", "diamond", "spade"]
+const values = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
 const deck = []
 let shuffledDeck = [];
 
--- warGame.js
+++ warGame.js
@@ -1,10 +1,26 @@
 import {shuffledDeck, createDeck, shuffleDeck} from "./deck.js";
 
 let playerDeck = [];
+let pDeckCount = playerDeck.length;
 let computerDeck = [];
-let playerPile = [];
-let computerPile = []; 
+let cDeckCount = computerDeck.length;
+let playedPile = []; 
 
+const CARD_VALUES = {
+    "2" : 2,
+    "3" : 3,
+    "4" : 4,
+    "5" : 5,
+    "6" : 6,
+    "7" : 7,
+    "8" : 8,
+    "9" : 9,
+    "10" : 10,
+    "J" : 11,
+    "Q" : 12,
+    "K" : 13,
+    "A" : 14
+}
 const dealCards = () => {
     const midPoint = Math.ceil(shuffledDeck.length / 2)
 
@@ -13,79 +29,96 @@ const dealCards = () => {
     return {playerDeck, computerDeck}
 }
 
+
+
 function startGame() {
     shuffleDeck(createDeck());
     dealCards();
 
+    console.log(shuffledDeck)
+    console.log("Player 1 and Player 2 decks: ")
+    console.log(playerDeck, computerDeck);
+
 }
-
-startGame()
-
-console.log(shuffledDeck)
-console.log("Player 1 and Player 2 decks: ")
-console.log(playerDeck, computerDeck);
 
 const playRound = () =>{
-    
-    console.log(`Player plays ${playerCard.rank} of ${playerCard.suit}`);
-    console.log(`Computer plays ${computerCard.rank} of ${computerCard.suit}`);
+    let playerCard = playerDeck.shift();
+    let computerCard = computerDeck.shift();
+    playedPile.push(playerCard, computerCard);
+    updateDeckCount()
 
-    if(playerCard.value > computerCard.value){
-        console.log("Player wins the roud");
-        playerPile.push(playerCard, computerCard);
-        playerPile = [];
-        computerPile = [];   
-    }
-    else if(playerCard.value < computerCard.value){
-        console.log("Computer wins the round");
-        computerPile.push(playerCard, computerCard);
-        playerPile = [];
-        computerPile = [];
-    }
-    else(playerCard.value = computerCard.value){
-        console.log("It's a tie. Let's go to war!");
-        playerPile.push(playerCard);
-        computerPile.push(computerCard);
-    if (playerDeck.length < 4) {
-      playerDeck = [...playerPile];
-      playerPile = [];
-      shuffleDeck(playerDeck);
-    }
-    if (computerDeck.length < 4) {
-      computerDeck = [...computerPile];
-      computerPile = [];
-      shuffleDeck(computerDeck);
-    }
-  }
+
+    console.log(`Player plays ${playerCard.value} of ${playerCard.suit}`);
+    console.log(`Computer plays ${computerCard.value} of ${computerCard.suit}`);
+
+    checkRoundWinner(playerCard, computerCard);
 };
-function goWar(){
-    while(!warWinner){
-     let playerWarDeck =  playerDeck.splice(0, 3);
-     let computerWarDeck = computerDeck.splice(0, 3);
-     const playerCard = playerDeck.shift();
-     const computerDeck = computerDeck.shift();
-     let warDeck = warDeck.push(playerWarDeck, computerDeck, playerCard, computerCard);
-     
-     if(playerCard)
-     playerDeck.push(pl)
-     computerDeck = [];
+
+function checkRoundWinner(playerCard, computerCard) {
+    let card1 = CARD_VALUES[playerCard.value]
+    let card2 = CARD_VALUES[computerCard.value]
+
+    if(card1 > card2) {
+        playerDeck = playerDeck.concat(playedPile)
+        playedPile = [];
+        updateDeckCount()
+        console.log("Player wins the roud");
+
+    } else if(card1 < card2) {
+        computerDeck = computerDeck.concat(playedPile)
+        playedPile = [];
+        updateDeckCount()
+        console.log("Computer wins the round");
+
+    } else {
+        console.log("It's a tie. Let's go to war!");
+        goWar();
+
     }
-
-
 }
-function cleanRound(){ 
-    playerDeck = playerDeck.concat(playerPile);
-    computerDeck = computerDeck.concat(computerPile);
 
-    playerPile = [];
-    computerPile = [];
+function goWar(){
+        // let playerWarDeck =  playerDeck.splice(0, 2);
+        // let computerWarDeck = computerDeck.splice(0, 2);
+        playedPile = playedPile.concat(playerDeck.splice(0, 3), computerDeck.splice(0, 3));
+        gameOverCheck();
+        updateDeckCount()
+        playRound();
+  
+}
+
+function cleanRound(){ 
 
     updateDeckCount()
+
+}
+
+function gameOverCheck() {
+    if (playerDeck.length < 4) {
+        playerDeck = [...playerPile];
+        playerPile = [];
+        shuffleDeck(playerDeck);
+    }
+    if (computerDeck.length < 4) {
+        computerDeck = [...computerPile];
+        computerPile = [];
+        shuffleDeck(computerDeck);
+    }
 }
 
 function updateDeckCount(){
-
+    pDeckCount = playerDeck.length;
+    cDeckCount = computerDeck.length;
+    console.log(`Player deck ${pDeckCount}`)
+    console.log(`Computer deck ${cDeckCount}`)
+    console.log(`Pile ${playedPile.length}`)
 }
+
+const button = document.getElementById('startGame');
+button.addEventListener('click', startGame);
+
+const button2 = document.getElementById('nextRound');
+button2.addEventListener('click', playRound);
 
 /*const playGame = () => {
   shuffleDeck(createDeck());
